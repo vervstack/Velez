a74c690ef4e08e69e5c8b645952ecc196a320d1c
AlexSkilled <olegthander@yandex.ru>
2024-04-27 12:49:20 +0300

    [release] smth wrong with previous container + minor adjustments to launch script

--- start/run_velez.sh
+++ start/run_velez.sh
@@ -1,7 +1,13 @@
+export VELEZ_PORT_GRPC=53890
+
+docker network create verv
 
 docker run \
+  -d \
   --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -p 8090:53890 \
+  -v /tmp/velez:/tmp/velez \
+  -p ${VELEZ_PORT_GRPC}:53890 \
   --name velez \
-  --network verv godverv/velez:v0.1.15
+  --network verv \
+  godverv/velez:v0.1.16

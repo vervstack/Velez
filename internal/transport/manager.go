f5a2b8015099da72516bce586df737a9ba65d214
AlexSkilled <olegthander@yandex.ru>
2024-10-15 12:33:53 +0300

    "rscli auto-commit"

--- internal/transport/manager.go
+++ internal/transport/manager.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	errors "github.com/Red-Sock/trace-errors"
-	"github.com/sirupsen/logrus"
 	"github.com/soheilhy/cmux"
 	"golang.org/x/sync/errgroup"
 )
@@ -18,8 +17,6 @@ type ServersManager struct {
 
 	grpcServer
 	httpServer
-
-	portRO string
 }
 
 func NewServerManager(ctx context.Context, port string) (*ServersManager, error) {
@@ -32,8 +29,6 @@ func NewServerManager(ctx context.Context, port string) (*ServersManager, error)
 	httpMux := http.NewServeMux()
 
 	s := &ServersManager{
-		portRO: port,
-
 		ctx: ctx,
 		mux: mainMux,
 
@@ -46,7 +41,7 @@ func NewServerManager(ctx context.Context, port string) (*ServersManager, error)
 
 func (m *ServersManager) Start() error {
 	errGroup, ctx := errgroup.WithContext(context.Background())
-	logrus.Infof("accepting connections at: http://0.0.0.0%s", m.portRO)
+
 	errGroup.Go(m.mux.Serve)
 	errGroup.Go(m.grpcServer.start)
 	errGroup.Go(m.httpServer.start)

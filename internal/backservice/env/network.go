d3cf8e6b0f9bcbd8c40e764d0ef208e54efc4dda
AlexSkilled <olegthander@yandex.ru>
2024-12-27 10:59:26 +0300

    [VERV-115] ignore network connectio if already connected

--- internal/backservice/env/network.go
+++ internal/backservice/env/network.go
@@ -2,46 +2,23 @@ package env
 
 import (
 	"context"
-	"os"
-	"strings"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
-	"github.com/sirupsen/logrus"
-	errors "go.redsock.ru/rerrors"
-	"go.redsock.ru/toolbox"
+	"go.redsock.ru/rerrors"
 
-	"github.com/godverv/Velez/internal/clients/docker/dockerutils/list_request"
+	"github.com/godverv/Velez/internal/clients/docker/dockerutils"
 )
 
 const (
-	VervNetwork = "verv"
+	VervNetwork       = "verv"
+	VelezNetworkAlias = "velez"
 )
 
 func StartNetwork(dockerAPI client.CommonAPIClient) error {
 	ctx := context.Background()
-	args := list_request.New()
-	args.Name(VervNetwork)
-
-	networks, err := dockerAPI.NetworkList(ctx, types.NetworkListOptions{
-		Filters: args.Args(),
-	})
+	err := dockerutils.CreateNetwork(ctx, dockerAPI, VervNetwork)
 	if err != nil {
-		return errors.Wrap(err, "error listing networks")
-	}
-	if len(networks) == 0 {
-		logrus.Debug("Verv network not found. Creating...")
-		createNetReq := network.CreateOptions{
-			Internal:   true,
-			Attachable: true,
-		}
-
-		_, err = dockerAPI.NetworkCreate(ctx, VervNetwork, createNetReq)
-		if err != nil {
-			return errors.Wrap(err, "error creating network")
-		}
-		logrus.Debug("Verv network created")
+		return rerrors.Wrap(err, "error creating network")
 	}
 
 	contId := GetContainerId()
@@ -49,51 +26,15 @@ func StartNetwork(dockerAPI client.CommonAPIClient) error {
 		return nil
 	}
 
-	logrus.Debugf("Verv container at: %s", *contId)
-	cont, err := dockerAPI.ContainerInspect(ctx, *contId)
-	if err != nil {
-		return errors.Wrap(err, "error getting Velez container info")
+	connReq := dockerutils.ConnectToNetworkRequest{
+		NetworkName: VervNetwork,
+		ContId:      *contId,
+		Aliases:     []string{VelezNetworkAlias},
 	}
-
-	connection := &network.EndpointSettings{
-		Aliases: []string{"velez"},
-	}
-	err = dockerAPI.NetworkConnect(ctx, VervNetwork, cont.Name, connection)
+	err = dockerutils.ConnectToNetwork(ctx, dockerAPI, connReq)
 	if err != nil {
-		return errors.Wrap(err, "error connecting this instance to network")
+		return rerrors.Wrap(err, "error connecting velez to network")
 	}
 
 	return nil
-}
-
-var instanceContainerId *string
-
-// IsInContainer - function to determine weather
-// this instance ran inside a container or as a standalone app
-// returns container uuid if so
-func IsInContainer() bool {
-	return GetContainerId() != nil
-}
-
-func GetContainerId() *string {
-	if instanceContainerId == nil {
-		instanceContainerId = getContainerId()
-		if instanceContainerId == nil {
-			instanceContainerId = toolbox.ToPtr("")
-		}
-	}
-
-	if *instanceContainerId == "" {
-		return nil
-	}
-
-	return instanceContainerId
-}
-
-func getContainerId() *string {
-	hm, err := os.ReadFile("/etc/hostname")
-	if err != nil {
-		return nil
-	}
-	return toolbox.ToPtr(strings.TrimRight(string(hm), "\n"))
 }
--- internal/clients/docker/dockerutils/network.go
+++ internal/clients/docker/dockerutils/network.go
@@ -4,20 +4,21 @@ import (
 	"context"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
-	errors "go.redsock.ru/rerrors"
+	"go.redsock.ru/rerrors"
 
 	"github.com/godverv/Velez/internal/clients/docker/dockerutils/list_request"
 )
 
-func CreateNetworkSoft(ctx context.Context, d client.CommonAPIClient, networkName string) error {
+func CreateNetwork(ctx context.Context, d client.CommonAPIClient, networkName string) error {
 	f := list_request.New()
 	f.Name(networkName)
 	net, err := d.NetworkList(ctx, types.NetworkListOptions{
 		Filters: f.Args(),
 	})
 	if err != nil {
-		return errors.Wrap(err, "error inspecting network for service")
+		return rerrors.Wrap(err, "error inspecting network for service")
 	}
 
 	for _, item := range net {
@@ -28,9 +29,35 @@ func CreateNetworkSoft(ctx context.Context, d client.CommonAPIClient, networkNam
 
 	_, err = d.NetworkCreate(ctx,
 		networkName,
-		types.NetworkCreate{})
+		network.CreateOptions{})
 	if err != nil {
-		return errors.Wrap(err, "error creating network for service")
+		return rerrors.Wrap(err, "error creating network for service")
+	}
+
+	return nil
+}
+
+type ConnectToNetworkRequest struct {
+	NetworkName, ContId string
+	Aliases             []string
+}
+
+func ConnectToNetwork(ctx context.Context, d client.CommonAPIClient, req ConnectToNetworkRequest) error {
+	cont, err := d.ContainerInspect(ctx, req.ContId)
+	if err != nil {
+		return rerrors.Wrap(err, "error getting Velez container info")
+	}
+
+	isNetworkConnected := cont.NetworkSettings != nil && cont.NetworkSettings.Networks[req.NetworkName] != nil
+
+	if !isNetworkConnected {
+		connection := &network.EndpointSettings{
+			Aliases: req.Aliases,
+		}
+		err = d.NetworkConnect(ctx, req.NetworkName, cont.Name, connection)
+		if err != nil {
+			return rerrors.Wrap(err, "error connecting this instance to network")
+		}
 	}
 
 	return nil

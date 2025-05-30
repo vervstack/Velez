c95e479a1dfdfbbf77b77548b1ec88e063ca1760
AlexSkilled <olegthander@yandex.ru>
2025-05-30 14:01:35 +0300

    [master] forbid to self-upgrade

--- internal/transport/velez_api_impl/smerd_upgrade.go
+++ internal/transport/velez_api_impl/smerd_upgrade.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"go.redsock.ru/rerrors"
+	"google.golang.org/grpc/codes"
 
+	"go.vervstack.ru/Velez/internal/backservice/env"
 	"go.vervstack.ru/Velez/internal/domain"
 	"go.vervstack.ru/Velez/pkg/velez_api"
 )
@@ -12,10 +14,22 @@ import (
 func (a *Impl) UpgradeSmerd(ctx context.Context,
 	req *velez_api.UpgradeSmerd_Request) (*velez_api.UpgradeSmerd_Response, error) {
 
+	id := env.GetContainerId()
+	if id != nil {
+		smerd, err := a.smerdService.InspectSmerd(ctx, req.Name)
+		if err != nil {
+			return nil, rerrors.Wrap(err)
+		}
+		if smerd.Uuid == *id {
+			return nil, rerrors.NewUserError("Can't perform self upgrade", codes.Unimplemented)
+		}
+	}
+
 	upgradeReq := domain.UpgradeSmerd{
 		Name:  req.Name,
 		Image: req.Image,
 	}
+
 	err := a.pipeliner.UpgradeSmerd(upgradeReq).Run(ctx)
 	if err != nil {
 		return nil, rerrors.Wrap(err)

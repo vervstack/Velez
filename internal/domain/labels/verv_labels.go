4a96c97d817b7b4f1b802fbc7711d5eec8a3e70c
AlexSkilled <olegthander@yandex.ru>
2025-07-13 21:45:44 +0300

    [master] using yaml-matreshka parser

--- internal/domain/labels/verv_labels.go
+++ internal/domain/labels/verv_labels.go
@@ -1,6 +1,18 @@
 package labels
 
+import (
+	"github.com/docker/docker/api/types/image"
+)
+
 const (
 	CreatedWithVelezLabel = "CREATED_WITH_VELEZ"
 	MatreshkaConfigLabel  = "MATRESHKA_CONFIG_ENABLED"
 )
+
+func IsMatreshkaImage(r *image.InspectResponse) bool {
+	if r == nil || r.Config == nil || len(r.Config.Labels) == 0 {
+		return false
+	}
+
+	return r.Config.Labels[MatreshkaConfigLabel] == "true"
+}
--- internal/pipelines/assemble_config.go
+++ internal/pipelines/assemble_config.go
@@ -7,9 +7,11 @@ import (
 	"github.com/stretchr/testify/assert/yaml"
 	"go.redsock.ru/evon"
 	"go.redsock.ru/rerrors"
+	"go.vervstack.ru/matreshka/pkg/matreshka"
 	"go.vervstack.ru/matreshka/pkg/matreshka_api"
 
 	"go.vervstack.ru/Velez/internal/domain"
+	"go.vervstack.ru/Velez/internal/domain/labels"
 	"go.vervstack.ru/Velez/internal/pipelines/steps"
 	"go.vervstack.ru/Velez/pkg/velez_api"
 )
@@ -36,25 +38,22 @@ func (p *pipeliner) AssembleConfig(req domain.AssembleConfig) Runner[domain.AppC
 			steps.GetConfigFromContainerStep(p.nodeClients, p.services, &createReq, &contId, imageResp, configMount),
 			steps.DropContainerStep(p.nodeClients, &contId),
 		},
-		getResult: func() (*domain.AppConfig, error) {
+		getResult: func() (appConfig *domain.AppConfig, err error) {
 			configMount.Meta.Name = strings.ReplaceAll(configMount.Meta.Name, configFetchingPostfix, "")
-			appConfig := domain.AppConfig{
+			appConfig = &domain.AppConfig{
 				Meta:       configMount.Meta,
 				ContentRaw: configMount.Content,
 			}
 			switch appConfig.Meta.Format {
 			case matreshka_api.Format_yaml:
-				m := map[string]any{}
-				err := yaml.Unmarshal(configMount.Content, &m)
-				if err != nil {
-					return nil, rerrors.Wrap(err, "error unmarshalling from yaml to map")
+				if labels.IsMatreshkaImage(imageResp) {
+					appConfig.Content, err = fromMatreshkaYamlToEvon(configMount.Content)
+				} else {
+					appConfig.Content, err = fromYamlToEvon(configMount.Content)
 				}
-
-				appConfig.Content, err = evon.MarshalEnv(m)
 				if err != nil {
-					return nil, rerrors.Wrap(err, "error marshalling yaml to env")
+					return nil, rerrors.Wrap(err, "error parsing from yaml to evon")
 				}
-
 			case matreshka_api.Format_env:
 				err := evon.Unmarshal(configMount.Content, &appConfig.Content)
 				if err != nil {
@@ -62,7 +61,37 @@ func (p *pipeliner) AssembleConfig(req domain.AssembleConfig) Runner[domain.AppC
 				}
 			}
 
-			return &appConfig, nil
+			return appConfig, nil
 		},
 	}
+}
+
+func fromMatreshkaYamlToEvon(content []byte) (*evon.Node, error) {
+	c := matreshka.NewEmptyConfig()
+	err := c.Unmarshal(content)
+	if err != nil {
+		return nil, rerrors.Wrap(err, "error unmarshalling to matreshka config")
+	}
+
+	env, err := evon.MarshalEnv(c)
+	if err != nil {
+		return nil, rerrors.Wrap(err, "error marshalling to env")
+	}
+
+	return env, nil
+}
+
+func fromYamlToEvon(content []byte) (*evon.Node, error) {
+	m := map[string]any{}
+	err := yaml.Unmarshal(content, &m)
+	if err != nil {
+		return nil, rerrors.Wrap(err, "error unmarshalling from yaml to map")
+	}
+
+	env, err := evon.MarshalEnv(m)
+	if err != nil {
+		return nil, rerrors.Wrap(err, "error marshalling yaml to env")
+	}
+
+	return env, nil
 }

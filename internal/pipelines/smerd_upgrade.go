274068cda7fb235c752e36f932554f4788ca6996
AlexSkilled <olegthander@yandex.ru>
2025-07-26 08:13:11 +0300

    [master] correct work with configuration suffix

--- internal/pipelines/smerd_upgrade.go
+++ internal/pipelines/smerd_upgrade.go
@@ -9,6 +9,8 @@ import (
 	"go.vervstack.ru/Velez/internal/pipelines/steps"
 )
 
+const configSuffix = "_configuration_fetcher"
+
 func (p *pipeliner) UpgradeSmerd(req domain.UpgradeSmerd) Runner[any] {
 	newLaunch := domain.LaunchSmerd{}
 	img := image.InspectResponse{}
@@ -29,14 +31,24 @@ func (p *pipeliner) UpgradeSmerd(req domain.UpgradeSmerd) Runner[any] {
 			steps.PauseContainer(p.nodeClients, &oldContId),
 			// Config stage
 			steps.SingleFunc(func(_ context.Context) error {
-				newLaunch.Name = req.Name + "_configuration_fetcher"
+				newLaunch.Name = req.Name + configSuffix
 				return nil
 			}),
 			steps.CreateContainer(p.nodeClients, &newLaunch, &newContId),
 			steps.GetConfigFromContainerStep(p.nodeClients, p.services, &newLaunch, &newContId, &img, &cfgMount),
 			steps.DropContainerStep(p.nodeClients, &newContId),
+			// Config stage
+			steps.SingleFunc(func(_ context.Context) error {
+				newLaunch.Name = req.Name
+				return nil
+			}),
 			// Deploy stage
 			steps.PrepareVervConfig(p.nodeClients, p.services, &newLaunch, &img),
+			// Config stage
+			steps.SingleFunc(func(_ context.Context) error {
+				newLaunch.Name = req.Name + "_new"
+				return nil
+			}),
 			steps.CreateContainer(p.nodeClients, &newLaunch, &newContId),
 			steps.StartSmerd(p.nodeClients, &newContId),
 			steps.Healthcheck(p.nodeClients, &newLaunch, &newContId),

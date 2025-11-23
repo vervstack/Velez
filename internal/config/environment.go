0bc1121205ebd10f48a9dc001cefaefd4958d700
AlexSkilled <olegthander@yandex.ru>
2025-11-23 12:02:37 +0300

    "rscli auto-commit"

--- internal/config/environment.go
+++ internal/config/environment.go
@@ -7,7 +7,6 @@ type EnvironmentConfig struct {
 	CPUDefault         float64
 	CustomPassToKey    string
 	DisableAPISecurity bool
-	HeadscaleImage     string
 	IsHeadtailEnabled  bool
 	LogFormat          string
 	LogLevel           string
